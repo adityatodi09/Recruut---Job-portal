b83601b23736588a93d92efda567d80144a996d5
Aditya Todi <adityatodi.surat@gmail.com>
2024-12-16 23:23:09 +0530

    layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,57 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import AppLayout from './layouts/app-layout'
-import LandingPage from './pages/LandingPage'
-import MyJobs from './pages/MyJobs'
-import SavedJobs from './pages/SavedJobs'
-import PostJob from './pages/PostJob'
-import JobPage from './pages/JobPage'
-import JobListing from './pages/JobListing'
-import Onboarding from './pages/Onboarding'
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/LandingPage";
+import MyJobs from "./pages/MyJobs";
+import SavedJobs from "./pages/SavedJobs";
+import PostJob from "./pages/PostJob";
+import JobPage from "./pages/JobPage";
+import JobListing from "./pages/JobListing";
+import Onboarding from "./pages/Onboarding";
+import { ThemeProvider } from "./components/ui/theme-provider";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
-    children : [
+    children: [
       {
-        path: '/',
-        element: <LandingPage />
+        path: "/",
+        element: <LandingPage />,
       },
       {
-        path: '/my-jobs',
-        element: <MyJobs />
+        path: "/my-jobs",
+        element: <MyJobs />,
       },
       {
-        path: '/saved-jobs',
-        element: <SavedJobs />
+        path: "/saved-jobs",
+        element: <SavedJobs />,
       },
       {
-        path: '/post-jobs',
-        element: <PostJob />
+        path: "/post-jobs",
+        element: <PostJob />,
       },
       {
-        path: '/job/:id',
-        element: <JobPage />
+        path: "/job/:id",
+        element: <JobPage />,
       },
       {
-        path: '/jobs',
-        element: <JobListing />
+        path: "/jobs",
+        element: <JobListing />,
       },
       {
-        path: '/onboarding',
-        element: <Onboarding />
+        path: "/onboarding",
+        element: <Onboarding />,
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 function App() {
-  return <RouterProvider router={router} /> 
+  return(
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <RouterProvider router={router} />
+  </ThemeProvider>
+  );
 }
 
-export default App
-
+export default App;
