ea53d3ed9f9564e1af802fcb82a55186af927f92
Aditya Todi <adityatodi.surat@gmail.com>
2024-12-16 19:05:00 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './layouts/app-layout'
+import LandingPage from './pages/LandingPage'
+import MyJobs from './pages/MyJobs'
+import SavedJobs from './pages/SavedJobs'
+import PostJob from './pages/PostJob'
+import JobPage from './pages/JobPage'
+import JobListing from './pages/JobListing'
+import Onboarding from './pages/Onboarding'
 
-const App = () => {
-  return (
-    <div>
-      hello
-    </div>
-  )
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children : [
+      {
+        path: '/',
+        element: <LandingPage />
+      },
+      {
+        path: '/my-jobs',
+        element: <MyJobs />
+      },
+      {
+        path: '/saved-jobs',
+        element: <SavedJobs />
+      },
+      {
+        path: '/post-jobs',
+        element: <PostJob />
+      },
+      {
+        path: '/job/:id',
+        element: <JobPage />
+      },
+      {
+        path: '/jobs',
+        element: <JobListing />
+      },
+      {
+        path: '/onboarding',
+        element: <Onboarding />
+      },
+    ]
+  }
+])
+function App() {
+  return <RouterProvider router={router} /> 
 }
 
 export default App
+
