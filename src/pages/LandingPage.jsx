e20e9937a0cb7f8fbdeccb0cd0e6a42e5b79bce9
Aditya Todi <adityatodi.surat@gmail.com>
2024-12-25 03:13:54 +0530

    Landing Page developed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,21 @@
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import React from "react";
 import { Link } from "react-router-dom";
+import companies from "../data/companies.json";
+import faqs from "../data/faq.json";
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 function LandingPage() {
   return (
@@ -18,17 +33,74 @@ function LandingPage() {
           </span>
         </h1>
         <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
-          Explore thousands of job listings or find the perfect candidate 
+          Explore thousands of job listings or find the perfect candidate
         </p>
       </section>
-      <div>
+      <div className="flex gap-6 justify-center items-center">
         <Link to="/jobs">
-          <Button>Find Jobs</Button>
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
         </Link>
         <Link to="/post-jobs">
-          <Button>Post Jobs</Button>
+          <Button variant="destructive" size="xl">
+            Post Jobs
+          </Button>
         </Link>
       </div>
+
+      <Carousel
+        plugins={[Autoplay({ delay: 2000, stopOnInteraction: true })]}
+        className="w-full py-18"
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => {
+            return (
+              <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+                <img
+                  src={path}
+                  alt={name}
+                  className="h-9 sm:h-14 object-contain"
+                />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
+
+      <img src="/Banner.png" alt="Banner" className="w-full" />
+
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>For Job Seekers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Search and apply for jobs, track applications, and more.
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>For Employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Post jobs, manage applications, and find the best candidates.
+          </CardContent>
+        </Card>
+      </section>
+
+      <Accordion type="single" collapsible>
+        {faqs.map((faq, index) => {
+          return (
+            <AccordionItem key={index} value={`item-${index + 1}`}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>
+                {faq.answer}
+              </AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
     </main>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
   plugins: [react({fastRefresh: false })],
+  // esbuild: {
+  //   loader: 'jsx', // Ensure all `.js` files are treated as JSX files
+  //   include: [
+  //     // Add paths to the node_modules package that contains JSX
+  //     'node_modules/lucide-react/**',
+  //   ],
+  //   exclude: [],
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
