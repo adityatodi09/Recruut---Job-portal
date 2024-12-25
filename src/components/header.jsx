291e0c0fece42976c991f82e3775c84d9fdd7bc7
Aditya Todi <adityatodi.surat@gmail.com>
2024-12-26 05:09:42 +0530

    user onboarding completed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,3 @@
-
 import { Link, useSearchParams } from "react-router-dom";
 import { Button } from "./ui/button";
 import {
@@ -7,15 +6,17 @@ import {
   SignIn,
   SignInButton,
   UserButton,
+  useUser,
 } from "@clerk/clerk-react";
 import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const Header = () => {
-
   const [showSignIn, setShowSignIn] = useState(false);
 
   const [search, setSearch] = useSearchParams();
+
+  const { user } = useUser();
 
   useEffect(() => {
     if (search.get("sign-in")) {
@@ -23,12 +24,12 @@ const Header = () => {
     }
   }, [search]);
 
-  const handleOverlayClick=(e)=>{
+  const handleOverlayClick = (e) => {
     if (e.target === e.currentTarget) {
-      setShowSignIn(false); 
+      setShowSignIn(false);
       setSearch({});
     }
-  }
+  };
   return (
     <>
       <nav className="py-4 flex justify-between items-center">
@@ -37,28 +38,34 @@ const Header = () => {
         </Link>
         <div className="flex gap-8">
           <SignedOut>
-            <Button variant="outline" onClick={() => setShowSignIn(true)}> Login </Button>
+            <Button variant="outline" onClick={() => setShowSignIn(true)}>
+              {" "}
+              Login{" "}
+            </Button>
           </SignedOut>
           <SignedIn>
-            <Link to="/post-jobs">
-              <Button variant="destructive" className="rounded-full">
-                <PenBox size={20} className="mr-2" />
-                Post a Job
-              </Button>
-            </Link>
-            <UserButton 
-            appearance={{
-              elements: {
-                avatarBox: "w-10 h-10",
-              },
-            }}>
+            {user?.unsafeMetadata.role?.role === "recruiter" && (
+              <Link to="/post-jobs">
+                <Button variant="destructive" className="rounded-full">
+                  <PenBox size={20} className="mr-2" />
+                  Post a Job
+                </Button>
+              </Link>
+            )}
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
               <UserButton.MenuItems>
-                <UserButton.Link 
+                <UserButton.Link
                   label="My Jobs"
                   labelIcon={<BriefcaseBusiness size={15} />}
                   href="/my-jobs"
                 />
-                <UserButton.Link 
+                <UserButton.Link
                   label="Saved Jobs"
                   labelIcon={<Heart size={15} />}
                   href="/saved-jobs"
@@ -69,13 +76,17 @@ const Header = () => {
         </div>
       </nav>
 
-      {showSignIn && (<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-      onClick={handleOverlayClick}>
-        <SignIn 
-          signUpForceRedirectUrl="/onboarding"
-          fallbackRedirectUrl="/onboarding"
+      {showSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
+            signUpForceRedirectUrl="/onboarding"
+            fallbackRedirectUrl="/onboarding"
           />
-      </div>)}
+        </div>
+      )}
     </>
   );
 };
